54c57711f5ee3ee2ca203a6dc5ab1d5845f3484b
NicoleSoltsov <113133856+NicoleSoltsov@users.noreply.github.com>
2025-03-13 11:02:44 -0400

    htht

---
+++ src/components/IngredientItem.vue
@@ -0,0 +1,47 @@
+<script setup>
+import ingredientImages from '@/assets/ingredientImages';
+
+defineProps(['name']);
+</script>
+
+<template>
+  <div class="box">
+    <img :src="ingredientImages[name]" :alt="name" />
+    <div>
+      <p class="text">{{ name }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.box {
+    background-color: #FFD0D7;
+    border-style: solid;
+    border-width: 3px;
+    border-color: lightpink;
+    justify-content: center;
+    overflow: hidden;
+    width: 200px;
+    height: 300px;
+    margin-top: 35px;
+    margin-left: 35px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+}
+
+.box img {
+    width: 80%;
+    height: auto;
+    border: 3px solid #FF87AB;
+    margin-top: 10px;
+}
+
+.text {
+    margin-top: 10px;
+    font-family: "Patrick Hand", cursive;
+    font-size: 30px;
+    color: darkmagenta;
+}
+</style>
---
+++ src/components/IngredientList.vue
@@ -0,0 +1,23 @@
+<script setup>
+import IngredientItem from './IngredientItem.vue';
+import ingredientImages from '@/assets/ingredientImages.js';
+
+// Get ingredient names dynamically from ingredientImages.js
+const ingredients = Object.keys(ingredientImages);
+</script>
+
+<template>
+  <div class="ingredient-list">
+    <IngredientItem v-for="ingredient in ingredients" :key="ingredient" :name="ingredient" />
+  </div>
+</template>
+
+<style scoped>
+.ingredient-list {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    justify-content: center;
+    padding: 20px;
+}
+</style>
