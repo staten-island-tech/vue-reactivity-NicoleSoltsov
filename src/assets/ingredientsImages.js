421f6e6619fb6db337d7143c17b8b8eb9589f087
NicoleSoltsov <113133856+NicoleSoltsov@users.noreply.github.com>
2025-03-18 11:01:10 -0400

    dgfd

--- src/assets/ingredientsImages.js
+++ src/assets/ingredientsImages.js
@@ -1,23 +1,21 @@
 const ingredientImages = {
-    strawberries: new URL('@/assets/images/strawberries.avif', import.meta.url).href,
-    chocolate_sauce: new URL('@/assets/images/chocsauce.jpg', import.meta.url).href,
-    bananas: new URL('@/assets/images/bananas.jpg', import.meta.url).href,
-    whipped_cream: new URL('@/assets/images/whippedcream.jpg', import.meta.url).href,
-    blackberries: new URL('@/assets/images/blackberries.avif', import.meta.url).href,
-    blueberries: new URL('@/assets/images/blueberries.jpg', import.meta.url).href,
-    caramel: new URL('@/assets/images/caramel.avif', import.meta.url).href,
-    chocolate_chips: new URL('@/assets/images/choclatechips.jpg', import.meta.url).href,
-    hazelnuts: new URL('@/assets/images/hazelnuts.jpg', import.meta.url).href,
-    honey: new URL('@/assets/images/honey.jpg', import.meta.url).href,
-    maple_syrup: new URL('@/assets/images/maplesyrup.jpg', import.meta.url).href,
-    nutella: new URL('@/assets/images/nutella.jpg', import.meta.url).href,
-    peaches: new URL('@/assets/images/peaches.jpg', import.meta.url).href,
-    peanut_butter: new URL('@/assets/images/peanutbutter.webp', import.meta.url).href,
-    powdered_sugar: new URL('@/assets/images/powderedsugar.jpg', import.meta.url).href,
-    raspberries: new URL('@/assets/images/raspberries.jpg', import.meta.url).href,
-    sprinkles: new URL('@/assets/images/sprinkles.avif', import.meta.url).href,
-    peaches: new URL('@/assets/images/peaches.jpg', import.meta.url).href,
-  };
-  
-  export default ingredientImages;
-  
+  strawberries: new URL('/src/assets/images/strawberries.avif', import.meta.url).href,
+  chocolate_sauce: new URL('/src/assets/images/chocsauce.jpg', import.meta.url).href,
+  bananas: new URL('/src/assets/images/bananas.jpg', import.meta.url).href,
+  whipped_cream: new URL('/src/assets/images/whippedcream.jpg', import.meta.url).href,
+  blackberries: new URL('/src/assets/images/blackberries.avif', import.meta.url).href,
+  blueberries: new URL('/src/assets/images/blueberries.jpg', import.meta.url).href,
+  caramel: new URL('/src/assets/images/caramel.avif', import.meta.url).href,
+  chocolate_chips: new URL('/src/assets/images/choclatechips.jpg', import.meta.url).href,
+  hazelnuts: new URL('/src/assets/images/hazelnuts.jpg', import.meta.url).href,
+  honey: new URL('/src/assets/images/honey.jpg', import.meta.url).href,
+  maple_syrup: new URL('/src/assets/images/maplesyrup.jpg', import.meta.url).href,
+  nutella: new URL('/src/assets/images/nutella.jpg', import.meta.url).href,
+  peaches: new URL('/src/assets/images/peaches.jpg', import.meta.url).href,
+  peanut_butter: new URL('/src/assets/images/peanutbutter.webp', import.meta.url).href,
+  powdered_sugar: new URL('/src/assets/images/powderedsugar.jpg', import.meta.url).href,
+  raspberries: new URL('/src/assets/images/raspberries.jpg', import.meta.url).href,
+  sprinkles: new URL('/src/assets/images/sprinkles.avif', import.meta.url).href,
+};
+
+export default ingredientImages;
--- src/components/CrepeDisplay.vue
+++ src/components/CrepeDisplay.vue
@@ -1,20 +1,27 @@
 <script setup>
-defineProps(['selectedIngredients']);
+import { computed } from "vue";
+
+defineProps(["selectedIngredients"]);
+
+const getImagePath = (ingredient) => {
+  return new URL(`/src/assets/ingredientImages/${ingredient}.png`, import.meta.url).href;
+};
 </script>
 
 <template>
   <div class="crepe-container">
     <div class="crepe">
-      <img v-if="selectedIngredients.length === 0" src="@/assets/empty-crepe.png" alt="Empty crepe" class="crepe-base"/>
-      
+      <img v-if="selectedIngredients.length === 0" src="@/assets/images/empty-crepe.webp" alt="Empty crepe" class="crepe-base"/>
+
       <div v-for="(ingredient, index) in selectedIngredients" :key="index" class="ingredient">
-        <img :src="`@/assets/ingredientImages/${ingredient}.png`" :alt="ingredient" />
+        <img :src="getImagePath(ingredient)" :alt="ingredient" />
       </div>
     </div>
 
     <p v-if="selectedIngredients.length === 0" class="empty-message">Your crepe is empty!</p>
   </div>
 </template>
+
 
 <style scoped>
 .crepe-container {
